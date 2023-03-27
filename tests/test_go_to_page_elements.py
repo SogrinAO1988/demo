59916dc9af5858a847b7951e05497e5f6784152c
Aleksandr_Olegovich <sogrin1988@yandex.ru>
2023-03-27 18:34:52 +0300

    first commit

--- tests/test_go_to_page_elements.py
+++ tests/test_go_to_page_elements.py
@@ -3,7 +3,7 @@ from pages.elements_page import ElementsPage
 
 
 def test_go_to_page_elements(browser):
-
+    # 22
     demo_qa_page = DemoQa(browser)
     elements_page = ElementsPage(browser)
     demo_qa_page.visit()
